306b5868cddebb0d2b815596c1ea8414fdbbc0ca
ryan9206 <ryan9206@hotmail.com>
2017-07-12 10:23:41 -0700

    Main page with bootstrap

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const express = require('express')
 const app = express()
 const path = require('path')
 
+app.use(express.static(path.join(__dirname, 'browser_components')))
+
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + '/index.html'))
+  res.sendFile(path.join(__dirname + '/RyanHome.html'))
 })
+
+app.get('/')
 
 app.listen(3000, function () {
   console.log('RyanHome is listening on port 3000!')
