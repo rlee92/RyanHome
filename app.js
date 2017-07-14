74d9e795d22222c791d36f9d836d871989330681
ryan9206 <ryan9206@hotmail.com>
2017-07-13 20:59:58 -0700

    Created 'header.html' for modulization

--- app.js
+++ app.js
@@ -2,13 +2,31 @@ const express = require('express')
 const app = express()
 const path = require('path')
 
-app.use(express.static(path.join(__dirname, 'browser_components')))
+app.use(express.static(path.join(__dirname, '')))
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/RyanHome.html'))
 })
 
-app.get('/')
+// Personal projects read more routes
+app.get('/swapem2', function (req, res){
+  res.sendFile(path.join(__dirname + '/contents/personalPJT/swapem2.html'))
+})
+
+app.get('/contact', function (req, res){
+  res.sendFile(path.join(__dirname + '/contents/personalPJT/contact.html'))
+})
+
+
+// Academic projects read more routes
+app.get('/swapem1', function (req, res){
+  res.sendFile(path.join(__dirname + '/contents/academicPJT/swapem1.html'))
+})
+
+app.get('/fvt', function (req, res){
+  res.send('Hello')
+})
+
 
 app.listen(3000, function () {
   console.log('RyanHome is listening on port 3000!')
